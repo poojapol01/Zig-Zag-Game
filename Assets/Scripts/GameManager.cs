e844c6c590ff82c998b882886a4a6ad629908512
poojapol01 <polpooja05@gmail.com>
2023-02-06 15:28:38 +0530

    Added Score System UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public bool isGameStarted;
     private int score=0;
+    public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +28,7 @@ public class GameManager : MonoBehaviour
     public void IncreaseGameScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 
     public int getScoreCount()
