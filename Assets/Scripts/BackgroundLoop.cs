017b52ea4bfe9e5aa82125346fcdf3e0433d45af
poojapol01 <polpooja05@gmail.com>
2023-02-10 23:48:34 +0530

    Added Background Music to the game

---
+++ Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    public static BackgroundLoop instance;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
